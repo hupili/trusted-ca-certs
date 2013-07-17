429577f7792a393f18f3569c030e5612ad82a34b
HU Pili <hpl1989@gmail.com>
2013-07-17 18:34:41 +0800

    re-layout    * 'certs/*' refers to rel path    * Use separate 'sources' dir

--- init.sh
+++ init.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env sh
 
-src=`pwd`/"ca-certificates_20130610_all.deb"
-dst=`pwd`/"certs"
+src="sources"
+dst="certs"
 
 rm -rf $dst
 mkdir -p $dst
 cd $dst
 
-for path in `find $src -name "*.crt"`
+for path in `find ../$src -name "*.crt"`
 do
     fn=`basename $path`
     # c_rehash do not process *.crt...
